b3aa5468bf47fcab5ffa8e3fea1ec8cbe52d97e8
Tiny_Murky <murky0830@gmail.com>
2025-04-26 01:09:57 +0800

    greetingPrefix refactor

--- hello-world/main_test.go
+++ hello-world/main_test.go
@@ -4,20 +4,33 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Murky")
+		got := Hello("Murky", "")
 		want := "Hello, Murky"
 
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 
 		want := "Hello, World"
 
 		assertCorrectMessage(t, got, want)
 	})
 
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in ", func(t *testing.T) {
+		got := Hello("Jack", "French")
+		want := "Bonjour, Jack"
+
+		assertCorrectMessage(t, got, want)
+	})
 }
 
 func assertCorrectMessage(t testing.TB, got string, want string) {
