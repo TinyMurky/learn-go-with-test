5eeadd9ab7bccb8ff8d87cba49e012aa4b581443
Tiny_Murky <murky0830@gmail.com>
2025-04-26 00:51:17 +0800

    Hello, World with helper test

--- hello-world/main_test.go
+++ hello-world/main_test.go
@@ -7,9 +7,7 @@ func TestHello(t *testing.T) {
 		got := Hello("Murky")
 		want := "Hello, Murky"
 
-		if got != want {
-			t.Errorf("got %q, expect %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
@@ -17,9 +15,17 @@ func TestHello(t *testing.T) {
 
 		want := "Hello, World"
 
-		if got != want {
-			t.Errorf("got %q, expect %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 
+}
+
+func assertCorrectMessage(t testing.TB, got string, want string) {
+	// TB 可以滿足 *testing.T and *testing.B
+	t.Helper() // 這個是interface, 專門告訴tester這個function只是helper不是test
+	// 這樣錯誤的時候顯示的錯誤行數就會是test case的，不會跑進來
+
+	if got != want {
+		t.Errorf("got %q, expect %q", got, want)
+	}
 }
